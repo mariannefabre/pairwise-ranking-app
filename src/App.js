0115f9a6e9616ac1d7b815c0364a28ec0f46a206
MarianneF <marianne.fabre@hotmail.fr>
2021-03-15 13:05:09 +0100

    Add scroll to top on transition

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Ranking from "./components/Ranking";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Form from "./components/Form";
 import Home from "./components/Home";
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
   const survey = useSelector((state) => state.survey);
@@ -13,11 +14,17 @@ function App() {
     <Router>
       <div className="App">
         <h1>Pairwise Comparison</h1>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/form" component={Form} />
-          <Route exact path="/questionnaire" component={survey.isSurveyFinished ? Ranking : Choice} />
-        </Switch>
+        <ScrollToTop>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/form" component={Form} />
+            <Route
+              exact
+              path="/questionnaire"
+              component={survey.isSurveyFinished ? Ranking : Choice}
+            />
+          </Switch>
+        </ScrollToTop>
       </div>
     </Router>
   );
